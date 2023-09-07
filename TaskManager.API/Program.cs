af38c90b4d68c0c5cf324d04d01970393b60b497
Guilherme Cornacini <guilhermevcornacini@gmail.com>
2023-09-07 20:38:31 -0300

    removed startup.cs

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -1,4 +1,33 @@
-using TaskManager.API;
+using Microsoft.Extensions.Options;
+using TaskManager.API.Data.Configurations;
+using TaskManager.API.Data.Repositories;
 
-var builder = WebApplication.CreateBuilder(args)
-    .UseStartup<Startup>();
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.Configure<DatabaseConfig>(builder.Configuration.GetSection(nameof(DatabaseConfig)));
+builder.Services.AddSingleton<IDatabaseConfig>(sp => sp.GetRequiredService<IOptions<DatabaseConfig>>().Value);
+
+builder.Services.AddSingleton<ITasksRepository, TasksRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
